3ca7eb1998b787665be6f1f113f7cd7da7410822
hieu <hdtran89@yahoo.com>
2014-10-29 00:08:26 -0400

    add new files backend

--- index.php
+++ index.php
@@ -25,7 +25,29 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
+    <style>
+        .navbar-nav > li > a 
+        { 
+            padding-top: 5px !important; padding-bottom: 5px !important; 
+        }
+        .navbar 
+        { 
+            min-height: 28px !important; 
+        }
+        .navbar-brand 
+        { 
+            padding-top: 5px; padding-bottom: 10px; padding-left: 10px; 
+        }
+        .navbar.navbar-default
+        {
+            border-right-width: 0px;
+            border-top-width: 0px;
+            border-left-width: 0px;
+            border-bottom-width: 1px;
+            margin-bottom: 2px;
+            height: 30px;
+        }
+    </style>
 </head>
 
 <body>
@@ -64,14 +86,51 @@
             </ul>
         </div>
         <!-- /#sidebar-wrapper -->
-
+		<!-- Static navbar -->
+	      <div class="navbar navbar-default" role="navigation">
+	        <div class="container-fluid">
+	          <div class="navbar-header">
+	            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+	              <span class="sr-only">Toggle navigation</span>
+	              <span class="icon-bar"></span>
+	              <span class="icon-bar"></span>
+	              <span class="icon-bar"></span>
+	            </button>
+	            <i class="fa fa-bars" id="menu-toggle" style="font-size:24px"></i>
+	          </div>
+	          <div class="navbar-collapse collapse">
+	            <ul class="nav navbar-nav">
+	              <li class="active"><a href="#">Link</a></li>
+	              <li><a href="#">Link</a></li>
+	              <li><a href="#">Link</a></li>
+	              <li class="dropdown">
+	                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+	                <ul class="dropdown-menu" role="menu">
+	                  <li><a href="#">Action</a></li>
+	                  <li><a href="#">Another action</a></li>
+	                  <li><a href="#">Something else here</a></li>
+	                  <li class="divider"></li>
+	                  <li class="dropdown-header">Nav header</li>
+	                  <li><a href="#">Separated link</a></li>
+	                  <li><a href="#">One more separated link</a></li>
+	                </ul>
+	              </li>
+	            </ul>
+	            <ul class="nav navbar-nav navbar-right">
+	              <li class="active"><a href="./">Default</a></li>
+	              <li><a href="../navbar-static-top/">Static top</a></li>
+	              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+	            </ul>
+	          </div><!--/.nav-collapse -->
+	        </div><!--/.container-fluid -->
+	      </div>
         <!-- Page Content -->
         <div id="page-content-wrapper">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
 
-                    <i class="fa fa-bars fa-3x" id="menu-toggle"></i>
+                    
                             <?php
                                 switch($_GET['content']){
                                     case 'register': include("partials/register.php");
